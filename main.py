1966ab8a5d8c3e2cb812789747638388f8ff99e9
shameel17 <ashameel646@gmail.com>
2023-04-12 12:47:17 +0530

    multiple table code

---
+++ main.py
@@ -0,0 +1,15 @@
+cont = True
+while cont:
+    a = int(input("Which table do you want: "))
+    b = int(input("Which value do you want: "))
+    print(a,'*',b, '=',a*b)
+    c = input("Do you want to continue: (yes/no) ")
+    if c == "yes":
+        a
+    else:
+        cont = False
+        print("see you")
+
+
+
+
